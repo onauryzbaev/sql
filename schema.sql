ce2b93060b99157c96968805c50df5f6f045e76a
onauryzbaev <150703047+onauryzbaev@users.noreply.github.com>
2023-11-21 21:55:11 +0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,17 +1,9 @@
-DROP TABLE IF EXISTS posts, authors;
-
-CREATE TABLE authors (
+CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
-);
-
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    author_id INTEGER REFERENCES authors(id) NOT NULL,
-    title TEXT  NOT NULL,
+    title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    created_at BIGINT NOT NULL
+    author_id INTEGER NOT NULL,
+    author_name VARCHAR(255) NOT NULL,
+    created_at BIGINT NOT NULL,
+    published_at BIGINT NOT NULL
 );
-
-INSERT INTO authors (id, name) VALUES (0, 'Дмитрий');
-INSERT INTO posts (id, author_id, title, content, created_at) VALUES (0, 0, 'Статья', 'Содержание статьи', 0);
